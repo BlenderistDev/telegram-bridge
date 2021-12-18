b814ad5457aaccc45f182431d35c7fa41a81e400
blenderistDev <demonx.blenderist@gmail.com>
2021-12-19 00:36:07 +0300

    add auth storage

---
+++ src/telegram/auth/auth.ts
@@ -0,0 +1,26 @@
+import EventEmitter = require("events");
+
+export class Auth {
+
+    private emitter: EventEmitter
+
+    public constructor() {
+        this.emitter = new EventEmitter()
+    }
+
+    public login(phone: string) {
+        this.emitter.emit("phone", phone)
+    }
+
+    public sing(code: string) {
+        this.emitter.emit("code", code)
+    }
+
+    public getPhone() {
+        return new Promise<string>(resolve => this.emitter.on("phone", (phone: string) => resolve(phone)))
+    }
+
+    public getCode() {
+        return new Promise<string>(resolve => this.emitter.on("code", (phone: string) => resolve(phone)))
+    }
+}

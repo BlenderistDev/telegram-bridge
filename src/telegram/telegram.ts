7a2f32eb1b2773db8e0277eb9194004ad18060b7
blenderistDev <demonx.blenderist@gmail.com>
2021-12-19 00:36:36 +0300

    add telegram client

---
+++ src/telegram/telegram.ts
@@ -0,0 +1,45 @@
+import {TelegramClient} from "telegram";
+import {StringSession} from "telegram/sessions";
+import {Auth} from "./auth/auth";
+import {loadSession, saveSession} from "./auth/session";
+
+export class Telegram {
+    private client: TelegramClient
+    private auth: Auth
+
+    public constructor() {
+        this.auth = new Auth()
+        this.initClient()
+    }
+
+    private async initClient() {
+        const apiId = parseInt(process.env.APP_ID)
+        const apiHash = process.env.APP_HASH
+
+        const stringSession = new StringSession(loadSession())
+
+        this.client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 });
+
+        await this.client.start({
+            phoneNumber: async () => await this.auth.getPhone(),
+            phoneCode: async () => await this.auth.getCode(),
+            onError: (err) => console.log(err),
+        });
+
+        saveSession(<string><unknown>this.client.session.save())
+    }
+
+    public login(phone: string) {
+        this.auth.login(phone)
+    }
+
+    public sign(code: string) {
+        this.auth.sing(code)
+    }
+
+    public getMe() {
+        if (this.client.connected) {
+            return this.client.getMe()
+        }
+    }
+}

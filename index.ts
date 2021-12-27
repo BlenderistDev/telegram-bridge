9f09c025f6dddbb1264f8e264fc3d91b70913a6e
blenderistDev <demonx.blenderist@gmail.com>
2021-12-27 19:52:19 +0300

    refactoring

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import {Telegram} from "./src/telegram/telegram"
-import {startServer} from "./src/grpc/server"
-import {loadEnv} from "./src/config/config"
-import { KafkaProducer } from "./src/kafka/kafka";
+import { Telegram } from './src/telegram/telegram'
+import { startServer } from './src/grpc/server'
+import { loadEnv } from './src/config/config'
+import { KafkaProducer } from './src/kafka/kafka';
 
 (async function () {
-    loadEnv()
-    const kafkaProducer = await KafkaProducer.init();
-    const telegram = await Telegram.createTelegramClient(kafkaProducer)
-    startServer(telegram)
+  loadEnv()
+  const kafkaProducer = await KafkaProducer.init()
+  const telegram = await Telegram.createTelegramClient(kafkaProducer)
+  startServer(telegram)
 })()

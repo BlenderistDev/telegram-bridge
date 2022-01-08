bea35221c1686f15f4093a7e0d50b70416cbc03a
blenderistDev <demonx.blenderist@gmail.com>
2022-01-08 22:17:29 +0300

    add MAX_MUTE_UNTIL for mute
    chat mutes not work without it

--- src/grpc/notifySettings/factory.ts
+++ src/grpc/notifySettings/factory.ts
@@ -1,7 +1,10 @@
 import { Api } from 'telegram'
 
+const MAX_MUTE_UNTIL = 2147483647
+
 export const mute = new Api.InputPeerNotifySettings({
-  silent: true
+  silent: true,
+  muteUntil: MAX_MUTE_UNTIL // for chat mute
 })
 
 export const unMute = new Api.InputPeerNotifySettings({
